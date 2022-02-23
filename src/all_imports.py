a689be8737b7dbb2a3478052b9a8ef456f0dda1c
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-02-23 22:20:04 +0530

    refactor: moved all imports and login_verifaction to one separate file.

---
+++ src/all_imports.py
@@ -0,0 +1,16 @@
+import tweepy
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+consumer_key = os.getenv('TWITTER_API_KEY')
+consumer_secret = os.getenv('TWITTER_API_KEY_SECRET')
+access_token = os.getenv('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
---
+++ test/verify_credentials.py
@@ -0,0 +1,24 @@
+import tweepy
+import os
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+consumer_key = os.getenv('TWITTER_API_KEY')
+consumer_secret = os.getenv('TWITTER_API_KEY_SECRET')
+access_token = os.getenv('TWITTER_ACCESS_TOKEN')
+access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+try:
+    print(api.verify_credentials())
+    print("Successfully logged in")
+except tweepy.TweepError as e:
+    print(e)
+except Exception as e:
+    print(e)
