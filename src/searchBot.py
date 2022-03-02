4909e760e651a33def12b73d9b685f0d172bfee6
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-03-03 00:33:36 +0530

    feat: Appreciate only if it contains 'day' and made bot to run every 500ms if stops.

--- src/searchBot.py
+++ src/searchBot.py
@@ -1,6 +1,6 @@
 from all_imports import *
 
-hashtag = '#100daysofcode'
+hashtag = '#100daysofcodechallenge'
 tweetnumber = 10
 
 tweets = tweepy.Cursor(api.search, hashtag).items(tweetnumber)
@@ -8,13 +8,15 @@ tweets = tweepy.Cursor(api.search, hashtag).items(tweetnumber)
 def searchBot():
     for tweet in tweets:
         try:
-            tweet.retweet()
-            api.create_favorite(tweet.id)
-            api.update_status('@' + tweet.entities['user_mentions'][0]['screen_name']+' Keep Going!!!  ' + tweet.entities['user_mentions'][0]['name'], tweet.id)
-            time.sleep(2)
+            if 'day' in tweet.text.lower():
+                tweet.retweet()
+                api.create_favorite(tweet.id)
+                api.update_status('@' + tweet.entities['user_mentions'][0]['screen_name']+' ☑️Keep Going!!! ' + tweet.entities['user_mentions'][0]['name'], tweet.id)
+            time.sleep(15)
         except tweepy.TweepError as e:
             print(e.reason)
-            time.sleep(2)
-
-searchBot()
+            time.sleep(15)
+while True:
+    searchBot()
+    time.sleep(500)
             
