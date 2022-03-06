05f654170386cfb152bf1466e4584782f92a531a
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-03-06 09:31:53 +0530

    feat: follow account with retweets having 'day' in tweets text.

--- src/searchBot.py
+++ src/searchBot.py
@@ -8,10 +8,11 @@ tweets = tweepy.Cursor(api.search, hashtag).items(tweetnumber)
 def searchBot():
     for tweet in tweets:
         try:
+            tweet.retweet()
+            api.create_favorite(tweet.id)
+            api.update_status('@' + tweet.entities['user_mentions'][0]['screen_name']+' ☑️Keep Going!!! ' + tweet.entities['user_mentions'][0]['name'], tweet.id)
             if 'day' in tweet.text.lower():
-                tweet.retweet()
-                api.create_favorite(tweet.id)
-                api.update_status('@' + tweet.entities['user_mentions'][0]['screen_name']+' ☑️Keep Going!!! ' + tweet.entities['user_mentions'][0]['name'], tweet.id)
+                api.create_friendship(tweet.entities['user_mentions'][0]['id'])
             time.sleep(15)
         except tweepy.TweepError as e:
             print(e.reason)
